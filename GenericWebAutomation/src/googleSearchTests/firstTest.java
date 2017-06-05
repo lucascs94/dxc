c49b545d2c76a5de929314bbfa09b0453c611a13
lucascs94 <lucasschefer.94@gmail.com>
2017-06-05 14:22:17 -0300

    commit teste.

---
+++ GenericWebAutomation/src/googleSearchTests/firstTest.java
@@ -0,0 +1,59 @@
+package googleSearchTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class firstTest {
+
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void DriverSetUp(){
+		System.setProperty("webdriver.gecko.driver", "C:/Users/carassai/Desktop/eclipse/geckodriver.exe");
+		driver =  new FirefoxDriver();
+		driver.get("http://google.com.br");
+		driver.manage().window().maximize();
+	}
+
+	//	@Before
+	//	public void TestSetUp(){
+	//		
+	//	}
+
+	@Test
+	public void testOne(){
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
+		//tira screenshot
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("lst-ib")));
+		modular.ModularMethods.tiraScreenshot(driver);
+
+		//executa a pesquisa
+		WebElement we = driver.findElement(By.id("lst-ib"));
+		we.sendKeys("testes automatizados com selenium");
+		we.submit();
+
+		//tira screenshot
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("hdtb-mitem")));
+		modular.ModularMethods.tiraScreenshot(driver);
+	}
+
+	//	@After
+	//	public void TestTearDown(){
+	//
+	//	}
+
+	@AfterClass
+	public static void DriverTearDown(){
+		//driver.quit();
+	}
+}
---
+++ GenericWebAutomation/src/modular/ModularMethods.java
@@ -0,0 +1,26 @@
+package modular;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class ModularMethods {
+	public static void tiraScreenshot(WebDriver driver){
+		SimpleDateFormat dateFormat = new SimpleDateFormat("DD-MM-YYYY/hh-mm-ssaa");
+		File scr = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String destfile = dateFormat.format(new Date()) + ".png";
+		try {
+			FileUtils.copyFile(scr, new File("C:/Users/carassai/Desktop/eclipse/Screenshots/"+destfile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
