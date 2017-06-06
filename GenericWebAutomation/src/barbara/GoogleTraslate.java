5100ec293ef7531459d69716ff9631a28b201677
moresba <moresba@SIBJ5HGHNZ.americas.hpqcorp.net>
2017-06-06 10:43:47 -0300

    Google Tradutor

--- GenericWebAutomation/src/barbara/GoogleTraslate.java
+++ GenericWebAutomation/src/barbara/GoogleTraslate.java
@@ -1,5 +1,41 @@
 package barbara;
 
-public class GoogleTraslate {
+import java.util.Properties;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleTraslate {
+	
+	private static WebDriver driver;
+	
+	@Before
+	public void setUp () {
+		Properties prop = modular.ModularMethods.getProperties();
+		System.setProperty("webdriver.gecko.driver", prop.getProperty("geckodriver.path"));
+		driver =  new FirefoxDriver();
+		driver.get(prop.getProperty("url1"));
+		driver.manage().window().maximize();
+	
+		
+	}
+	
+	
+	@Test
+	public void translate () {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("text")));
+		driver.findElement(By.name("text")).sendKeys("the book is on the table");;
+		driver.findElement(By.xpath("//div[2]/input")).click();
+		
+		
+	}
 }
+
+
