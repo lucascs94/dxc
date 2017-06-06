76ce0a35bed5126e07cae8e7fdcd27676cc5d0f5
moresba <moresba@SIBJ5HGHNZ.americas.hpqcorp.net>
2017-06-06 15:24:36 -0300

    Alterar idioma

--- GenericWebAutomation/src/barbara/GoogleTranslate.java
+++ GenericWebAutomation/src/barbara/GoogleTranslate.java
@@ -1,7 +1,9 @@
 package barbara;
 
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -10,7 +12,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class GoogleTraslate {
+public class GoogleTranslate {
 	
 	private static WebDriver driver;
 	
@@ -22,21 +24,36 @@ public class GoogleTraslate {
 		driver.get(prop.getProperty("url1"));
 		driver.manage().window().maximize();
 	
-		
 	}
 	
-	
+
 	@Test
 	public void translate () {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		
+		//Escrever texto a ser traduzido
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("text")));
 		driver.findElement(By.name("text")).sendKeys("the book is on the table");;
 		driver.findElement(By.xpath("//div[2]/input")).click();
 		
+		//Escutar leitura do texto
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[3]/div/div/span")));
 		driver.findElement(By.xpath("//div[3]/div/div/span")).click();
+		
+		//Alterara idiomas
+		driver.findElement(By.id("gt-swap")).click();
+		
 	}
+	
+	
+	//@After
+	  
+	  //public void TestTearDown(){
+		//WebDriverWait wait = new WebDriverWait(driver, 30);
+		//driver.quit();
+		//}
 }
+
+
 
 
