bc95f79aadcb0517999f735c0e2f89fee978157e
lucascs94 <lucasschefer.94@gmail.com>
2017-06-12 13:31:11 -0300

    Classe GoogleSearchTests alterada para utilizar TestNG

--- GenericWebAutomation/src/lucas/GoogleSearchTests.java
+++ GenericWebAutomation/src/lucas/GoogleSearchTests.java
@@ -1,70 +1,74 @@
 package lucas;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-@RunWith(Parameterized.class)
+//@RunWith(Parameterized.class)
 public class GoogleSearchTests {
 
-	private String searchContent;
-	private static Properties prop;
-	private static  WebDriver driver;
-	
+	//private String searchContent;
+	private Properties prop;
+	private WebDriver driver;
+
 	@BeforeClass
-	public static void DriverSetUp(){
+	public void DriverSetUp(){
 		prop = modular.ModularMethods.getProperties("config.properties");
 		modular.ModularMethods.setGeckodriverLocation();
 		driver =  new FirefoxDriver();
-		driver.manage().window().maximize();
+		//driver.get(prop.getProperty("url"));
+		driver.manage().window().maximize();	
 	}
 
-	@Before
+	@BeforeMethod
 	public void TestSetUp(){
 		driver.get(prop.getProperty("url"));
 	}
 
-	@After
+	@AfterMethod
 	public void TestTearDown(){
 
 	}
 
 	@AfterClass
-	public static void DriverTearDown(){
+	public void DriverTearDown(){
 		driver.quit();
 	}
-	
-	public GoogleSearchTests(String searchContent){
-		this.searchContent = searchContent;
-	}
-	
-	@Parameters
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {
-			{"Jose"},
-			{"Maria"},
-			{"João"},
-			{"Carla"}
-		});
-	}
-	
-	@Test
-	public void screenshotTest(){
+
+	//	public GoogleSearchTests(String searchContent){
+	//		this.searchContent = searchContent;
+	//	}
+
+	//	@Parameters
+	//	public static Collection<Object[]> data() {
+	//		return Arrays.asList(new Object[][] {
+	//			{"Jose"},
+	//			{"Maria"},
+	//			{"João"},
+	//			{"Carla"}
+	//		});
+	//	}
+
+
+	@DataProvider(name = "SearchContent")
+	public static Object[][] searchContent() {
+		return new Object[][] {{"joão"}, {"maria"}};
+	}	
+
+	@Test(dataProvider = "SearchContent")
+	public void screenshotTest(String searchContent){
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 
 		//tira screenshot
@@ -75,11 +79,12 @@ public class GoogleSearchTests {
 		WebElement we = driver.findElement(By.id("lst-ib"));
 		we.sendKeys(searchContent);
 		we.submit();
-		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
 		//tira screenshot
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("hdtb-mitem")));
+		wait.until(ExpectedConditions.elementToBeClickable(By.className("hdtb-mitem")));
 		modular.ModularMethods.tiraScreenshot(driver);
-		
+
 		we.clear();
 	}
 }
