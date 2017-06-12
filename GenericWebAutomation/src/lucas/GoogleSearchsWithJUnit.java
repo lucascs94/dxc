b0f3a7aa8c90d41f35f0ed3d2bb8e73e539f3fa1
lucascs94 <lucasschefer.94@gmail.com>
2017-06-12 17:49:41 -0300

    GoogleSearchsWithTestNG.java e GoogleSearchsWithJUnit.java adicionadas

---
+++ GenericWebAutomation/src/lucas/GoogleSearchsWithJUnit.java
@@ -0,0 +1,79 @@
+package lucas;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+@RunWith(Parameterized.class)
+public class GoogleSearchsWithJUnit {
+
+	private String searchContent;
+	private static Properties prop;
+	private static WebDriver driver;
+
+	public GoogleSearchsWithJUnit(String searchContent){
+		this.searchContent = searchContent;
+	}
+
+	@BeforeClass
+	public static void DriverSetUp(){
+		prop = modular.ModularMethods.getProperties("config.properties");
+		modular.ModularMethods.setGeckodriverLocation();
+		driver =  new FirefoxDriver();
+		driver.manage().window().maximize();	
+	}
+
+	@Before
+	public void TestSetUp(){
+		driver.get(prop.getProperty("url"));
+	}
+
+	@After
+	public void TestTearDown(){
+
+	}
+
+	@AfterClass
+	public static void DriverTearDown(){
+		driver.quit();
+	}
+
+	@Parameters
+	public static Collection<Object[]> searchContent() {
+		return Arrays.asList(new Object[][] {{"como automatizar testes com selenium"}, {"como automatizar testes com testng"}});
+	}	
+
+	@Test
+	public void screenshotTest(){
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+
+		//tira screenshot
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("lst-ib")));
+		modular.ModularMethods.tiraScreenshot(driver);
+
+		//executa a pesquisa usando dados parâmetrizados 
+		driver.findElement(By.id("lst-ib")).sendKeys(searchContent);
+		driver.findElement(By.name("btnG")).click();
+
+		//tira screenshot
+		wait.until(ExpectedConditions.elementToBeClickable(By.className("hdtb-mitem")));
+		modular.ModularMethods.tiraScreenshot(driver);
+	}
+}
---
+++ GenericWebAutomation/src/lucas/GoogleSearchsWithTestNG.java
@@ -0,0 +1,66 @@
+package lucas;
+
+import java.util.Properties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class GoogleSearchsWithTestNG {
+
+	private Properties prop;
+	private WebDriver driver;
+
+	@BeforeClass
+	public void DriverSetUp(){
+		prop = modular.ModularMethods.getProperties("config.properties");
+		modular.ModularMethods.setGeckodriverLocation();
+		driver =  new FirefoxDriver();
+		driver.manage().window().maximize();	
+	}
+
+	@BeforeMethod
+	public void TestSetUp(){
+		driver.get(prop.getProperty("url"));
+	}
+
+	@AfterMethod
+	public void TestTearDown(){
+
+	}
+
+	@AfterClass
+	public void DriverTearDown(){
+		driver.quit();
+	}
+
+	@DataProvider(name = "SearchContent")
+	public static Object[][] searchContent() {
+		return new Object[][] {{"como automatizar testes com selenium"}, {"como automatizar testes com testng"}};
+	}	
+
+	@Test(dataProvider = "SearchContent")
+	public void screenshotTest(String searchContent){
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+
+		//tira screenshot
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("lst-ib")));
+		modular.ModularMethods.tiraScreenshot(driver);
+
+		//executa a pesquisa usando dados parâmetrizados 
+		driver.findElement(By.id("lst-ib")).sendKeys(searchContent);
+		driver.findElement(By.name("btnG")).click();
+
+		//tira screenshot
+		wait.until(ExpectedConditions.elementToBeClickable(By.className("hdtb-mitem")));
+		modular.ModularMethods.tiraScreenshot(driver);
+	}
+}
