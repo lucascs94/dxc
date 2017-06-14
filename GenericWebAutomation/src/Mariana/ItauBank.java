2123258e071c795627c6eba04533885c06420f9a
gomesbor <marianagb.borges@gmail.com>
2017-06-14 10:26:14 -0300

    Ajustes na classe ItauBank; Exclusão da classe de parametrização

--- GenericWebAutomation/src/Mariana/ItauBank.java
+++ GenericWebAutomation/src/Mariana/ItauBank.java
@@ -1,10 +1,14 @@
 package Mariana;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,51 +16,59 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.runners.Parameterized;
 
+
+
+@RunWith(Parameterized.class) 
 public class ItauBank {
 
+	
+	private String agencia;
+	private String conta;
+	
 	private static WebDriver driver;
 	Properties prop2 = modular.ModularMethods.getProperties("massa_dados.txt");
 	
 	@Before
-		public void setUp () {
-		Properties prop = modular.ModularMethods.getProperties("configMariana.txt");
-		modular.ModularMethods.setChromeDriverLocation();
-		driver = new ChromeDriver();
-		driver.get(prop.getProperty("url"));
-		driver.manage().window().maximize();
-	
+	public void preCondicao() {
+		
+			Properties prop = modular.ModularMethods.getProperties("configMariana.txt");
+			modular.ModularMethods.setChromeDriverLocation();
+			driver = new ChromeDriver();
+			driver.get(prop.getProperty("url"));
+			driver.manage().window().maximize();
+			
 	}
 	
-	@Test
-	//Acessar conta - parte 1
-	public void acessarConta(){
-	WebDriverWait wait = new WebDriverWait(driver, 30);
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("campo_agencia")));
+	public ItauBank(String conta, String agencia) {
+		this.agencia = agencia;
+		this.conta = conta;
+	}
 	
-		WebElement ag = driver.findElement(By.id("campo_agencia"));
-		ag.sendKeys(prop2.getProperty("agencia1"));
-		
-		WebElement numConta = driver.findElement(By.id("campo_conta")); 
-		numConta.sendKeys(prop2.getProperty("conta1"));
-		
-		WebElement botaoAcessar = driver.findElement(By.className("btnSubmit")); 
-		botaoAcessar.click();
-		
-		//
-}
+	//@Test
+	
+	
+	
 	
 	@Test
-	//Alternar valores no botão de acesso:
+	public void acessarConta()
+		{
+			driver.findElement(By.id("campo_agencia")).sendKeys(agencia);
+			driver.findElement(By.id("campo_conta")).sendKeys(conta);
+		
+		}
 	
-	
-	
-	
-	
+	@Parameters(name = "{index}: {0}|{1}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			{"6319", "135304"}
+		}); 
+	}
 	
 	@AfterClass
-	public static void DriverTearDown(){
-		driver.quit();
+	public static void Final(){
+		//driver.quit();
 	}
 }
 
